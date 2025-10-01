b195f51a289ea791fe70bf6a944ae82ab9193892
HallanCosta <hallan.costa1@hotmail.com>
2025-09-30 21:19:52 -0300

    fix(server): fix import account

--- apps/server/src/modules/account/index.ts
+++ apps/server/src/modules/account/index.ts
@@ -1,6 +1,5 @@
 export { AccountType, AccountConnection } from './AccountType';
 export { Account } from './AccountModel';
 export { AccountLoader } from './AccountLoader';
-export { accountMutations } from './mutations/accountMutations';
 export { accountConnectionField } from './accountFields';
 export * from './accountService';
